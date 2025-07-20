6a2f1b2d393f5dbe7e39a094d539d049b3d21495
Tommy <you@example.com>
2025-07-20 14:52:04 +0500

    add blog_detail page

--- blog/views.py
+++ blog/views.py
@@ -12,11 +12,20 @@ def blog_list(req):
     return render(req, "blog/blog_list.html", context)
 
 
-def blog_detail(req, slug):
+def blog_detail(request, slug):
     post = get_object_or_404(BlogPost, slug=slug, is_published=True)
+
+    # Recent posts for sidebar
+    recent_posts = BlogPost.objects.filter(is_published=True).exclude(id=post.id)[:2]
+
+    # Related posts (last 3 posts excluding current)
+    related_posts = BlogPost.objects.filter(is_published=True).exclude(id=post.id)[:2]
 
     context = {
         "post": post,
+        "recent_posts": recent_posts,
+        "related_posts": related_posts,
     }
+    return render(request, "blog/blog_detail.html", context)
 
     return render(req, "blog/blog_detail.html", context)
